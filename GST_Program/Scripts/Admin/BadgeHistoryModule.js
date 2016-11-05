5801c91b297877bd34b740ab7a970979c2f944e7
CalgaryMichael <cseth.michael@gmail.com>
2016-11-05 17:13:31 -0500

    JS File Rearrange
    Rearranged how I was working the JS files. Now each page has a separate
    file so we dont just have one giant JS file for all of the Admin views

--- GST_Program/Scripts/Admin/BadgeHistoryModule.js
+++ GST_Program/Scripts/Admin/BadgeHistoryModule.js
@@ -1,4 +1,10 @@
-﻿window.BadgeHistoryModule = (function ($) {
+﻿$(document).ready(function () {
+    $('#searchIcon').click(function () {
+        submitForm();
+    });
+});
+
+window.BadgeHistoryModule = (function ($) {
     getGiver = function () {
         $.ajax({
             url: '/Admin/BadgeHistoryGiver',
@@ -62,72 +68,6 @@
         });
     };
 
-    getStudent = function () {
-        $.ajax({
-            url: '/Admin/PersonType',
-            type: 'POST',
-            dataType: 'html',
-            data: { type: "Student" },
-            beforeSend: function () {
-                $('#spinner').css('display', 'block');
-            },
-            success: function (data) {
-                $('#results').html(data);
-                $('#type-display').html("Student");
-            },
-            error: function (data) {
-                $('#results').html(data.responseText);
-            },
-            complete: function () {
-                $('#spinner').css('display', 'none');
-            }
-        });
-    };
-
-    getFaculty = function () {
-        $.ajax({
-            url: '/Admin/PersonType',
-            type: 'POST',
-            dataType: 'html',
-            data: { type: "Faculty" },
-            beforeSend: function () {
-                $('#spinner').css('display', 'block');
-            },
-            success: function (data) {
-                $('#results').html(data);
-                $('#type-display').html("Faculty");
-            },
-            error: function (data) {
-                $('#results').html(data.responseText);
-            },
-            complete: function () {
-                $('#spinner').css('display', 'none');
-            }
-        });
-    };
-
-    getProfessor = function () {
-        $.ajax({
-            url: '/Admin/PersonType',
-            type: 'POST',
-            dataType: 'html',
-            data: { type: "Professor" },
-            beforeSend: function () {
-                $('#spinner').css('display', 'block');
-            },
-            success: function (data) {
-                $('#results').html(data);
-                $('#type-display').html("Professor");
-            },
-            error: function (data) {
-                $('#results').html(data.responseText);
-            },
-            complete: function () {
-                $('#spinner').css('display', 'none');
-            }
-        });
-    };
-
     return {
         init: function () {
             $('#txtSearch').keypress(function (e) {
@@ -159,21 +99,3 @@ var submitForm = (function () {
         getBadge();
     }
 });
-
-$(document).ready(function () {
-    $('#searchIcon').click(function () {
-        submitForm();
-    });
-
-    $('#Student').click(function () {
-        getStudent();
-    });
-
-    $('#Faculty').click(function () {
-        getFaculty();
-    });
-
-    $('#Professor').click(function () {
-        getProfessor();
-    });
-})
---
+++ GST_Program/Scripts/Admin/IndexModule.js
@@ -0,0 +1,97 @@
+﻿$(document).ready(function () {
+    $('#searchIcon').click(function () {
+        submitForm();
+    });
+
+    $('#Student').click(function () {
+        getStudent();
+    });
+
+    $('#Faculty').click(function () {
+        getFaculty();
+    });
+
+    $('#Professor').click(function () {
+        getProfessor();
+    });
+});
+
+window.IndexModule = (function () {
+    getStudent = function () {
+        $.ajax({
+            url: '/Admin/PersonType',
+            type: 'POST',
+            dataType: 'html',
+            data: { type: "Student" },
+            beforeSend: function () {
+                $('#spinner').css('display', 'block');
+            },
+            success: function (data) {
+                $('#results').html(data);
+                $('#type-display').html("Student");
+            },
+            error: function (data) {
+                $('#results').html(data.responseText);
+            },
+            complete: function () {
+                $('#spinner').css('display', 'none');
+            }
+        });
+    };
+
+    getFaculty = function () {
+        $.ajax({
+            url: '/Admin/PersonType',
+            type: 'POST',
+            dataType: 'html',
+            data: { type: "Faculty" },
+            beforeSend: function () {
+                $('#spinner').css('display', 'block');
+            },
+            success: function (data) {
+                $('#results').html(data);
+                $('#type-display').html("Faculty");
+            },
+            error: function (data) {
+                $('#results').html(data.responseText);
+            },
+            complete: function () {
+                $('#spinner').css('display', 'none');
+            }
+        });
+    };
+
+    getProfessor = function () {
+        $.ajax({
+            url: '/Admin/PersonType',
+            type: 'POST',
+            dataType: 'html',
+            data: { type: "Professor" },
+            beforeSend: function () {
+                $('#spinner').css('display', 'block');
+            },
+            success: function (data) {
+                $('#results').html(data);
+                $('#type-display').html("Professor");
+            },
+            error: function (data) {
+                $('#results').html(data.responseText);
+            },
+            complete: function () {
+                $('#spinner').css('display', 'none');
+            }
+        });
+    };
+
+    return {
+        getStudent: function () {
+            getStudent();
+        },
+        getFaculty: function () {
+            getFaculty();
+        },
+        getProfessor: function () {
+            getProfessor();
+        },
+    };
+})(jQuery);
