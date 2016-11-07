cb661c8bc5eadc856f5f16d05d367fb7489862bb
amos-parrish <aparrish0314@gmail.com>
2016-11-06 22:07:21 -0600

    Grid View

--- GST_Program/Controllers/TreeController.cs
+++ GST_Program/Controllers/TreeController.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using GST_Program.Domain;
+using GST_Program.Models;
 
 namespace GST_Program.Controllers {
     public class TreeController : Controller {
-        
+
+        DatabaseModel database = new DatabaseModel();
+        List<BadgeReceived> badgeList = new List<BadgeReceived>();
+
 		// GET: Tree
         public ActionResult Index() {
             return View();
@@ -16,8 +20,10 @@ namespace GST_Program.Controllers {
 
 		// GET: Tree/GridView
 		public ActionResult GridView(String ID) {
+            badgeList = database.ReadAllBadgeReceivedByReceiver("10010");
+
 			// ID: used to get Badges received by Person with ID
-			return View();
+			return View(badgeList);
 		}
 
 
