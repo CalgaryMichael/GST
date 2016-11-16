c5e56c5f3932ebf17caa650f6ad89366c61f613d
Miles Manners <doctor.miles.manners@gmail.com>
2016-11-16 13:14:49 -0600

    Updated jQuery, Bootstrap. Added DataTables

--- GST_Program/App_Start/BundleConfig.cs
+++ GST_Program/App_Start/BundleConfig.cs
@@ -7,18 +7,29 @@ namespace GST_Program
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // jQuery
             bundles.Add(new ScriptBundle("~/bundles/js/jquery").Include(
-                "~/Scripts/jquery-{version}.js"));
+                "~/Content/jquery/jquery-{version}.min.js"));
 
+            // jQuery Validate (Remove?)
             bundles.Add(new ScriptBundle("~/bundles/js/jqueryval").Include(
                 "~/Scripts/jquery.validate*"));
 
+            // Bootstrap
             bundles.Add(new ScriptBundle("~/bundles/js/bootstrap").Include(
                 "~/Content/Vendor/bootstrap/js/bootstrap.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/css/bootstrap").Include(
+            bundles.Add(new StyleBundle("~/bundles/css/bootstrap").Include(
                 "~/Content/Vendor/bootstrap/css/bootstrap.min.css"));
 
+            // DataTables
+            bundles.Add(new ScriptBundle("~/bundles/js/datatables").Include(
+                "~/Content/Vendor/datatables/js/datatables.min.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/css/datatables").Include(
+                "~/Content/Vendor/datatables/css/datatables.min.css"));
+
+            // Font Awesome
             bundles.Add(new StyleBundle("~/bundles/css/fontawesome").Include(
                 "~/Content/Vendor/font-awesome/font-awesome.min.css"));
         }
